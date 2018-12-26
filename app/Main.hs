2d6f0a20c8686a5d1b31b2e85169eebabc8fc241
Gregory Propf <gpropf@gmail.com>
2018-12-26 12:58:06 -0800

    I'm going to scrap the module idea for now. There are no good ways to do it in Haskell it seems.

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,10 @@ import Data.List
 
 import Control.Monad
 import Lib
-import Params
+import ParamUtil
 -- import System.Plugins.Load
-import GHC
-import GhcPlugins
+--import GHC
+--import GhcPlugins
 import Language.Haskell.Interpreter
 
 {-
@@ -21,7 +21,7 @@ main :: IO [()]
 -- main = sequence $ (writeImageList params "testpic" [0,2.0..8.0])
 main = sequence [rnlm]
 
---testghc = defaultPlugin
+
 errorString :: InterpreterError -> String
 errorString (WontCompile es) = intercalate "\n" (header : map unbox es)
   where
@@ -45,23 +45,11 @@ rnlm = do r <- runInterpreter testHint
 
 testHint =
     do
-      say "Load ./src/Params.hs"
-      loadModules ["../src/Params.hs", "../src/ParamUtil.hs", "../src/Lib.hs"]
-      --oadModules ["../src/Lib.hs"]
-      emptyLine
-      say "Put the Prelude, Data.Map and *Params in scope"
-      say "Data.Map is qualified as M!"
-      setTopLevelModules ["Params","ParamUtil","Lib"]
-      setImportsQ [("Prelude", Nothing), ("Data.Map", Just "M")]
-      emptyLine
-      say "Now we can query the type of an expression"
-      --let params = "params"
-      --say $ "e.g. typeOf " ++ params
-      --say =<< typeOf params
-      --emptyLine 
-      --paramArg <- eval params
-      -- say $ show paramArg
-      runStmt "wil <- sequence $ (ParamUtil.writeImageList2 paramHash \"dyntest\" [1.0,2.0..99.0])"
-      --say $ show writeImageListFn
-      --let rng = "[0,2.0..8.0]"
+      say "Load ../src/Params.hs, ../src/ParamUtil.hs, ../src/Lib.hs"
+      -- loadModules ["../src/Params.hs", "../src/ParamUtil.hs", "../src/Lib.hs"]
+      -- loadModules ["Params", "ParamUtil", "Lib"]
+      loadModules ["Params"]      
+      setTopLevelModules ["Params"]
+     -- setImportsQ [(ParamUtil, Just "P")]
+      runStmt "wil <- sequence $ (ParamUtil.writeImageList2 paramHash \"dyntest\" [1.0,2.0..9.0])"
       emptyLine 
