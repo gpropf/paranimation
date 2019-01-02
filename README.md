d64d85a5bfc4c771b23d29f1d40bc0dcc5aac413
Gregory Propf <gpropf@gmail.com>
2019-01-01 16:28:33 -0800

    Rewrote Readme.md and made a few minor tweaks to cabal file.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # Paranimation: Parametric Animations
 
-This is a small library and app to generate computer graphics that evolve according to changing parameters. A good example would be a zoom into a Mandelbrot set where the bounds of the viewport change over time. We plan on having two main elements in this system. One might be called the framemaker, the other is the interpolator. The framemaker's job is to take an associative array of arbitrary parameters and generate one image. The interpolator uses a file describing the way these parameters change over time. This is probably just going to be a small Haskell program itself defining an AA that contains the same hash keys as the ones passed to the framemaker but also has some data showing how the parameters evolve. All parameters are ultimately functions of an abstract time index.
+
+## Description:
+
+This package defines a set of data types and functions designed to make
+it easier to create parameter driven animations. A good example of a
+parameter driven animation would one of those Mandelbrot set zoom
+movies that you can find all over the place now. Basically, what's
+happening is that the parameters defining the window within the set
+are changing slowly with the set being re-rendered each time.
+
+I wanted to abstract the process of generating the images for this
+type of movie out from the process of defining keyvalues and
+interpolating between them. Essentially I wanted to be able to define
+arbitray keyvalues for an arbitrary number of parameters of varied
+types that would ultimately depend on a single driving parameter
+(essentially time or frame number if you like). There would also an
+automated process for interpolating along the chain of keyvalues with
+the possibility of defining varied interpolator functions as well
+though initially I would provide only a linear interpolator function.
+
+We provide a command line tool (currently called 'paranimate-exe')
+that offers a fairly sophisticated set of command line features
+powered by the optparse-applicative command-line parser
+library. Running the program without options will show a help message
+that details what the various options do.
+
+## Modules
+
+The package is designed to be modular so that one can create new
+animations relatively easily by just filling in the algorithm that
+generates a single frame. Providing the changing parameters that may
+be needed to drive it is fully automated by the overall framework.
+
 
--- src/ModuleTemplate.hs
+++ src/ModuleTemplate.hs
@@ -16,6 +16,8 @@ import System.Random
 import Number.Complex
 import Algebra.Ring( C )
 
+numPoints = 150000
+
 -- testVertices = [(-1.0) +: 1, 1 +: 1, 1 +: (-1), (-1) +: (-1)]
 
 vp :: Viewport
@@ -28,12 +30,12 @@ paramHash = fromList ([("x", [(0.0, BV (-1.5)),(50.0, BV 1.5),(500.0, BV 1.2)])
                       , ("ll", [(0.0, BVC ((-0.2) +: (-1))),(500.0, BVC ((-5.0) +: (-2.0)))])
                       , ("ur", [(0.0, BVC ((0.2) +: 1)),(500.0, BVC ((4.0) +: 4.0))])
                       , ("lr", [(0.0, BVC ((0.2) +: (-1))),(500.0,BVC ((3.0) +: (-2.0)))])
-                      , ("t11", [(0.0, BVC ((-0.75) +: (0.5))),(500.0, BVC ((4.0) +: 4.0))])
-                      , ("t12", [(0.0, BVC ((2) +: (2.5))),(500.0, BVC ((4.0) +: 4.0))])
-                      , ("t13", [(0.0, BVC ((2.5) +: (1.25))),(500.0, BVC ((4.0) +: 4.0))])
-                      , ("t21", [(0.0, BVC ((-2) +: (-1))),(500.0, BVC ((4.0) +: 4.0))])
-                      , ("t22", [(0.0, BVC ((0.75) +: (-2.75))),(500.0, BVC ((4.0) +: 4.0))])
-                      , ("t23", [(0.0, BVC ((-3) +: (-3))),(500.0, BVC ((4.0) +: 4.0))])])
+                      , ("t11", [(0.0, BVC ((-0.75) +: (0.5))),(500.0, BVC ((-4.75) +: (-3.5)))])
+                      , ("t12", [(0.0, BVC ((2) +: (2.5))),(500.0, BVC ((-2) +: (-1.5)))])
+                      , ("t13", [(0.0, BVC ((2.5) +: (1.25))),(500.0, BVC ((-1.5) +: (-2.75)))])
+                      , ("t21", [(0.0, BVC ((-2) +: (-1))),(500.0, BVC ((4) +: (5)))])
+                      , ("t22", [(0.0, BVC ((0.75) +: (-2.75))),(500.0, BVC ((4.75) +: (1.25)))])
+                      , ("t23", [(0.0, BVC ((-3) +: (-3))),(500.0, BVC ((1) +: (1)))])])
 
 drawCircle x y =
   let colr = PixelRGBA8 255 0 0 255
@@ -43,28 +45,37 @@ drawCircle x y =
 
 
 drawCircleCmplx c =
-  let colr = PixelRGBA8 255 100 0 255
+  let (n,p) = c
+     -- ageClr = round $ ((fromInteger (numPoints-n)/numPoints) * 255)
+--      colr = PixelRGBA8 ageClr 150 0 100
+      colr = PixelRGBA8 77 150 0 70
   in
     withTexture (uniformTexture colr) $
-    fill $ circle (viewport2abs vp (Vec2 (real c) (imag c))) 1.5
+    fill $ circle (viewport2abs vp (Vec2 (real p) (imag p))) 2.0
 
 
-pickPoints :: (Ord t1, RandomGen t2, Algebra.Ring.C a, Floating a, Num t1,
-      Eq a) =>  T a -> t2 -> t1 -> [T a] -> [T a] -> [T a]
-pickPoints startP g n vertices pl =
+{-pickPoints :: (Ord t1, RandomGen t2, Algebra.Ring.C a, Floating a, Num t1,
+      Eq a) =>  T a -> t2 -> t1 -> [T a] -> [T a] -> [T a] -}
+pickPoints startP g n vertices groupShift pl =
   if n <= 0 then pl else
-    let (i,g') = randomR (0, length vertices - 1) g
-        d = (0.5 `scale` ((vertices !! i) - startP))
+    let numVs = (length vertices) `div` 2
+        --numVs = (length vertices)
+        (i,g') = randomR (0, numVs - 1) g
+        (pf,g'') = randomR (1::Int,10) g'
+        groupShift' = if pf < 3 then numVs else 0
+        groupShiftMod = (groupShift + groupShift') `mod` (numVs*2)
+        d = (0.5 `scale` ((vertices !! (i + groupShiftMod)) - startP))
+--        d = (0.5 `scale` ((vertices !! i) - startP))
         newP = startP + d
     in
-      pickPoints newP g' (n-1) vertices (newP:pl)
+      pickPoints newP g' (n-1) vertices groupShiftMod ((n,newP):pl)
 
     
 makeFrame :: Data.Map.Map [Char] [(Double, BV Double)]
   -> StdGen -> Double
   -> Codec.Picture.Image Codec.Picture.PixelRGBA8
 makeFrame paramHash g t = do
-  let white = PixelRGBA8 10 50 200 255     
+  let white = PixelRGBA8 10 50 210 255     
       (BV x) = interpolatedValue linearInterpolate t "x" paramHash
       (BV y) = interpolatedValue linearInterpolate t "y" paramHash
       (BVC ul) = interpolatedValue linearInterpolate t "ul" paramHash
@@ -86,7 +97,8 @@ makeFrame paramHash g t = do
           let vertices = [t11,t12,t13,t21,t22,t23]
               --sp = (0.0 :: Double) +: (0.0 :: Double)
               sp = t11
-              ps = pickPoints sp g 50000 vertices []
+              ps = pickPoints sp g numPoints vertices 0 []
+--          mapM_ drawCircleCmplx (reverse ps)
           mapM_ drawCircleCmplx ps
           --drawCircle x y
           --drawCircle (real ul) (imag ul)
